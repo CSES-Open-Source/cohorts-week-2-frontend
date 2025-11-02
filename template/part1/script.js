ea238eccf2c88f5b7f1955c761df3d6f0f7d94fd
ribellye@gmail.com <ribellye@gmail.com>
2025-11-02 13:40:13 -0800

    minor fixes for part 1

--- template/part1/script.js
+++ template/part1/script.js
@@ -1,10 +1,10 @@
 // TODO 10: add this script to index.html
 
-
+// TODO 11: make variables for each of the buttons and displays, the first is done for you
 const countDisplay = document.getElementById("count");
-const incrementBtn = document.getElementById("increment");
-const decrementBtn = document.getElementById("decrement");
-const resetBtn = document.getElementById("reset");
+const incrementBtn = document.getElementById("INSERT CORRECT ID HERE");
+const decrementBtn =  // INSERT CORRECT FUNCTION HERE
+// CREATE A NEW resetBtn const here
 
 let count = 0;
 
@@ -13,14 +13,10 @@ function updateDisplay() {
 }
 
 // TODO 11: Add click event to increment button
-incrementBtn.addEventListener("click", () => {
-  
-});
+incrementBtn.addEventListener("click", () => {});
 
 // TODO 12: Add click event to decrement button
-decrementBtn.addEventListener("click", () => {
-  
-});
+decrementBtn.addEventListener("click", () => {});
 
 // Click event to reset button
 resetBtn.addEventListener("click", () => {
